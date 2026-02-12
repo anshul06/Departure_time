1709d01101605111309b41237f8dde2acae442d7
anshul06 <anshulhiremath5@gmail.com>
2026-02-12 19:47:03 +0530

    Redirect root path to API documentation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from app.routes.departures import router
+from fastapi.responses import RedirectResponse
+
 
 app = FastAPI(title="Departure Times API")
 
 app.include_router(router)
+
+@app.get("/", include_in_schema=False)
+def root():
+    return RedirectResponse(url="/docs")
